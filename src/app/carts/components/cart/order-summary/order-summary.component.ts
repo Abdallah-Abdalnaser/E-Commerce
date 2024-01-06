a65ef1dcaf4e89389fb82d4227f472a86078c4d1
Abdallah abdalnaser <abdallahalbhare2002@gmail.com>
2024-01-07 00:13:04 +0200

    #finish design of cart

--- src/app/carts/components/cart/order-summary/order-summary.component.ts
+++ src/app/carts/components/cart/order-summary/order-summary.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IconDefinition, faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-order-summary',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./order-summary.component.scss']
 })
 export class OrderSummaryComponent {
-
+  faArrowRight:IconDefinition=faArrowRight;
 }
