9fbc23b70cecc971f1a87b91b492c3737706b02a
Abdallah abdalnaser <abdallahalbhare2002@gmail.com>
2024-05-15 20:39:34 +0300

    #

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { faSun, IconDefinition ,faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { WeatherService } from './weather.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls:['./app.component.scss']
 })
-export class AppComponent {
-  title = '7-E-Commerce';
+export class AppComponent implements OnInit {
+  faSun:IconDefinition=faSun;
+  faSpinner:IconDefinition=faSpinner;
+  temperature:number=0;
+  humidity:number=0;
+  rainFall:number=0;
+  light:boolean=false;
+  isFetching:boolean=true;
+
+  constructor(private WeatherAppService:WeatherService) {
+    setInterval(()=>{
+      this.updateWeatherData()
+    },1000)
+  }
+
+  ngOnInit(): void {
+    this.updateWeatherData();
+  }
+
+  updateWeatherData() {
+    this.isFetching=true;
+    this.WeatherAppService.temperatureSensor().subscribe(
+      (res) => {
+        this.temperature = Number(res);
+      }
+    );
+
+    this.WeatherAppService.Humidity().subscribe(
+      (res) => {
+        this.humidity = Number(res);
+      }
+    );
+
+    this.WeatherAppService.RainSensor().subscribe(
+      (res) => {
+        this.rainFall = Number(res);
+      }
+    );
+
+    this.WeatherAppService.lightSensor().subscribe(
+      (res) => {
+        this.light = Boolean(res);
+      }
+    )
+    this.isFetching = false;
+  }
+
+  checkLight():string {
+    if (this.light === true) {
+      return "gold";
+    }else {
+      return "#e7e8ea";
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserModule,provideClientHydration } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SharedModule } from './shared/shared.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { ProductsModule } from './products/products.module';
-import { CartsModule } from './carts/carts.module';
+import { NgCircleProgressModule } from 'ng-circle-progress';
 
 @NgModule({
   declarations: [
@@ -15,10 +13,22 @@ import { CartsModule } from './carts/carts.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule,
     FontAwesomeModule,
-    ProductsModule,
-    CartsModule
+    HttpClientModule,
+    NgCircleProgressModule.forRoot({
+      "radius": 60,
+      "space": -10,
+      "outerStrokeGradient": true,
+      "outerStrokeWidth": 10,
+      "innerStrokeWidth": 10,
+      "title": "UI",
+      "animateTitle": false,
+      "animationDuration": 1000,
+      "showUnits": false,
+      "showBackground": false,
+      "clockwise": false,
+      "startFromZero": false,
+      "lazy": true}),
   ],
   providers: [],
   bootstrap: [AppComponent]
